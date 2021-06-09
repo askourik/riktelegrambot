4b5d5f50bc6798fd367b1770d6ee4d4d0768c605
askourik <skourikhin@gmail.com>
2021-06-09 18:01:15 +0300

    added sensors to message body

--- mail.sh
+++ mail.sh
@@ -46,23 +46,53 @@ subj="Subject: This is a test"
 body1="Hi $recipient,"
 body2="This is a test. Mode: $mode. Period: $period"
 body3="Bye!"
+HOSTSTATE=$(busctl --no-pager call xyz.openbmc_project.State.Host /xyz/openbmc_project/state/host0 org.freedesktop.DBus.Properties Get ss xyz.openbmc_project.State.Host CurrentHostState | cut -c47-)
+P105_PCH_AUX=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/P105_PCH_AUX xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+P12V_AUX=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/P12V_AUX xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+P1V8_PCH=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/P1V8_PCH xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+P3V3=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/P3V3 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVCCIN_CPU1=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVCCIN_CPU1 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVCCIN_CPU2=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVCCIN_CPU2 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVCCIO_CPU1=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVCCIO_CPU1 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVCCIO_CPU2=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVCCIO_CPU2 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVDQ_ABC_CPU1=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVDQ_ABC_CPU1 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVDQ_ABC_CPU2=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVDQ_ABC_CPU2 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVDQ_DEF_CPU1=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVDQ_DEF_CPU1 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVDQ_DEF_CPU2=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVDQ_DEF_CPU2 xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
+PVNN_PCH_AUX=$(busctl --system introspect xyz.openbmc_project.ADCSensor /xyz/openbmc_project/sensors/voltage/PVNN_PCH_AUX xyz.openbmc_project.Sensor.Value | grep writable | cut -c54-66)
 
 echo $from  > /etc/rikmail/mail.txt
 echo $to  >> /etc/rikmail/mail.txt
 echo $subj  >> /etc/rikmail/mail.txt
 echo >> /etc/rikmail/mail.txt
 echo $body1  >> /etc/rikmail/mail.txt
+echo "HOST STATE = $HOSTSTATE" >> /etc/rikmail/mail.txt
+echo "P105_PCH_AUX = $P105_PCH_AUX" >> /etc/rikmail/mail.txt
+echo "P12V_AUX = $P12V_AUX" >> /etc/rikmail/mail.txt
+echo "P1V8_PCH = $P1V8_PCH" >> /etc/rikmail/mail.txt
+echo "P3V3 = $P3V3" >> /etc/rikmail/mail.txt
+echo "PVCCIN_CPU1 = $PVCCIN_CPU1" >> /etc/rikmail/mail.txt
+echo "PVCCIN_CPU2 = $PVCCIN_CPU2" >> /etc/rikmail/mail.txt
+echo "PVCCIO_CPU1 = $PVCCIO_CPU1" >> /etc/rikmail/mail.txt
+echo "PVCCIO_CPU = $PVCCIO_CPU2" >> /etc/rikmail/mail.txt
+echo "PVDQ_ABC_CPU1 = $PVDQ_ABC_CPU1" >> /etc/rikmail/mail.txt
+echo "PVDQ_ABC_CPU2 = $PVDQ_ABC_CPU2" >> /etc/rikmail/mail.txt
+echo "PVDQ_DEF_CPU1 = $PVDQ_DEF_CPU1" >> /etc/rikmail/mail.txt
+echo "PVDQ_DEF_CPU2 = $PVDQ_DEF_CPU2" >> /etc/rikmail/mail.txt
+echo "PVNN_PCH_AUX = $PVNN_PCH_AUX" >> /etc/rikmail/mail.txt
 echo $body2  >> /etc/rikmail/mail.txt
-echo $body3  >> /etc/rikmail/mail.txt
+#echo $body3  >> /etc/rikmail/mail.txt
 echo >> /etc/rikmail/mail.txt
 
-sleep 5
 
+if [ $recipient != "info@example.com" ]; then
+sleep 5
 /usr/sbin/postfix start
 sleep 10
 /usr/sbin/sendmail -t < /etc/rikmail/mail.txt
 sleep 10
 /usr/sbin/postfix stop
 sleep 10
+fi
 
 
