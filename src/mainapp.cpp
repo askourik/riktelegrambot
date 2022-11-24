8c8271a42cee2d64122cd232605de1094c6eb00b
askourik <askourik@users.noreply.github.com>
2022-11-24 22:20:05 +0300

    Update mainapp.cpp

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -15,80 +15,48 @@ int main()
     io.run();
 }
 */
-#include "config.h"
-#include <time.h>
-#include <sys/stat.h>
+#include <cstdlib>
+#include <cerrno>
 
-using namespace std;
-using namespace TgBot;
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[])
+{
+  if(argc < 2) {
+    std::cerr << "Example 11: Wrong number of arguments" << std::endl 
+	      << "Example 11: Usage: example12 url" 
+	      << std::endl;
+    return EXIT_FAILURE;
+  }
   
-  // argv[1] path to configuration directory
-  // argv[2] verbouse 1- True; 0-False
-
-  int _verbouse = (argv[2][0] != 48 ? 1: 0);
-
+  char *url = argv[1];
   
-  // Pass "/.../.../.../chad_ID/" as argument to this program
-  int PATH_LENGTH = strlen(argv[1]);
-
-  char * CONFIG_ENV_VARIABLES_PATH = (char *) malloc(sizeof(char) * PATH_LENGTH);
-  strcpy(CONFIG_ENV_VARIABLES_PATH, argv[1]);
-  
-  string token = "YOUR TOKEN HERE";
-  Bot bot(token);
-
-  unsigned long long int  _chat_id = get_folder_name(CONFIG_ENV_VARIABLES_PATH, PATH_LENGTH);
-
-
-  // Get directory files
-  directory_files * _files = list_files_in_directory(CONFIG_ENV_VARIABLES_PATH);
-
-  for(int i = 0; i < _files->_count; i++){
-
-    // Create and assign file path
-    char * _file_path = (char *) malloc(sizeof(CONFIG_ENV_VARIABLES_PATH) + SIZE_FILE_NAME);
-    strcpy(_file_path, CONFIG_ENV_VARIABLES_PATH);
-    strcat(_file_path, "/");
-    strcat(_file_path, _files->_p);
-
-    // Get File ne NO extension
-    char * file_name_no_extension = get_file_name(_files->_p);
-
-    // Get File last modified
-    long long int _last_modified = get_file_last_modified_time(_file_path);
-    //printf("\nLast Modified: [%lld], Name: [%s]\n", _last_modified, file_name_no_extension);
+  try {
+    curlpp::Cleanup cleaner;
+    curlpp::Easy request;
     
-    //Create environment variable with File Name
-    char _last_modified_char[11] ;
-    sprintf(_last_modified_char, "%lld", _last_modified);
-    create_environment_variable(file_name_no_extension, _last_modified_char);
-
-    //Fork process
-    int _new_pid = fork();
-    if(_new_pid < 0 ){
-      printf("ERROR creating new procees \n");
-    }
-    else if(_new_pid == 0)
-    {
-      printf("\n----------NEW Child process ( %d )----------\n", getpid());
-      printf("########################################################\n\n");
-      fflush(stdout); // flush output of child process
-      
-      send_message_to_chat(_chat_id, file_name_no_extension, &bot, _file_path, _verbouse);
-    }
-    else{
-      printf("\n\n########################################################");
-      printf("\n----------Parent process    ( %d )----------\n", getpid());
-    }
+    request.setOpt(new curlpp::options::Url(url)); 
+    request.setOpt(new curlpp::options::Verbose(true)); 
     
-    _files->_p += SIZE_FILE_NAME;
-  } 
+    std::list<std::string> header; 
+    header.push_back("Content-Type: application/octet-stream"); 
+    
+    request.setOpt(new curlpp::options::HttpHeader(header)); 
+    
+    request.setOpt(new curlpp::options::PostFields("abcd"));
+    request.setOpt(new curlpp::options::PostFieldSize(5));
+    
+    request.perform(); 
+  }
+  catch ( curlpp::LogicError & e ) {
+    std::cout << e.what() << std::endl;
+  }
+  catch ( curlpp::RuntimeError & e ) {
+    std::cout << e.what() << std::endl;
+  }
 
-  printf("\n\t( %d )Parent Process exiting\n", getpid());
-  
-  return 0;
+  return EXIT_SUCCESS;
 }
-
-//g++ mainapp.cpp riktekegram.hpp riktekegram.cpp -o telegram_bot --std=c++11 -I/usr/local/include -lTgBot -lboost_system -lssl -lcrypto -lpthread
