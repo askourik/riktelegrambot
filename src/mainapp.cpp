941fa50865528bbd8c4eac237f881a87f0135692
askourik <askourik@users.noreply.github.com>
2022-12-08 17:17:14 +0300

    Update mainapp.cpp

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -10,23 +10,27 @@
 
 int main(int argc, char *argv[])
 {
-  if(argc < 2) {
+  if(argc < 3) {
     std::cerr << "Example 11: Wrong number of arguments" << std::endl 
-	      << "Example 11: Usage: telegrambot msgfile" 
+	      << "Example 11: Usage: telegrambot chat_id msgfile" 
 	      << std::endl;
     return EXIT_FAILURE;
   }
   
-  char url[] = "https://api.telegram.org/bot5812037533:AAHYmDkoIbtYGBSFW7_-qmHu2hKtBV7YHlQ/sendMessage";//argv[1];
-  char *msgfile = argv[1];
+  char url[] = "https://api.telegram.org/bot5812037533:AAHYmDkoIbtYGBSFW7_-qmHu2hKtBV7YHlQ/sendMessage";
+  char *chat_id = argv[1];
+  char *msgfile = argv[2];
   FILE *f = fopen(msgfile,"rb");
   fseek(f,0,SEEK_END);
   long fsize = ftell(f);
+  long msgsize = strlen(argv[1])+fsize+14;
   fseek(f,0,SEEK_SET);
-  char *msg = (char *)malloc(fsize+29);
-  strcpy(msg,"chat_id=-1001765034687&text=");
-  fread(msg+28,fsize,1,f);
-  msg[fsize+28]=0;
+  char *msg = (char *)malloc(msgsize+1);
+  strcpy(msg,"chat_id=");
+  strcat(msg,chat_id);
+  strcat(msg,"&text=");
+  fread(msg+msgsize-fsize,fsize,1,f);
+  msg[msgsize]=0;
   fclose(f);
 	
   try {
